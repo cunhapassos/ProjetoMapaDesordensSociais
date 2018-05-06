d79aa5af21018a59303850a8b9669a7dc73e7f19
bruno <brunocordeiro180@gmail.com>
2018-05-06 18:13:28 -0300

    login adicionado

--- web/index.js
+++ web/index.js
@@ -2,6 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express(); //"app" criado para se usar o express
 var pg = require('knex')({client: 'pg'});
+var session = require('express-session');
+var md5 = require('md5');
+
+var sess;
+
+app.use(session({
+	secret : "shss",
+	proxy: true,
+    resave: true,
+    saveUninitialized: true
+}));
 
 //CONEXÃO COM O BANCO
 var knex = require('knex')({
@@ -15,16 +26,6 @@ var knex = require('knex')({
   }
 });
 
-knex('org_orgao').insert({
-	org_idorgao: '1234', 
-	org_nome : 'caesb', 
-	org_descricao : 'agua'
-}).then(function(){
-	knex.select().from('org_orgao').then(function(ORG_ORGAO){
-		console.log(ORG_ORGAO);
-	})
-})
-
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -32,16 +33,62 @@ app.set("view engine", "ejs");
 app.engine('html', require('ejs').renderFile);
 
 app.get("/", function(req,res){
+	sess = req.session;
 	res.render('welcome');
 });
 
 app.get("/login", function(req,res){
-	res.render('login');
+	sess = req.session;
+
+	if(!sess.email){
+		res.render('login', {failed : 0});
+	}
+	else{
+		res.render('login', {failed : 0});
+	}
+
+});
+
+			
+// console.log(aux);
+
+app.post("/login", function(req,res){
+	sess = req.session;
+
+	var senha = req.body.password;
+	var email = req.body.email;
+
+	senha = md5(senha);
+
+	var name = 0;
+
+	knex('usuario').where({
+		usu_email : email,
+		usu_senha : senha
+	}).select().then(function(usuario){
+		if(usuario.length <= 0){
+			res.render("login", {failed : 1});
+		}
+		else{
+			sess.email = email;
+			res.render("admin");
+		}
+	});
 });
 
 app.get("/admin", function(req,res){
-	res.render('admin');
+	sess = req.session;
+
+	if (sess.email) {
+		res.render('admin');
+	}
+	else{
+		res.render("login", {failed : 0});
+	}
 });
+
+var senha = md5("123456");
+console.log(senha);
 
 app.listen('3000', () => { //abrindo a aplicação na porta 3000
 	console.log("Server started");
