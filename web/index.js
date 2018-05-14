7d43f9fbb0c6951981111f8b188f3bbe6eeb484f
bruno <brunocordeiro180@gmail.com>
2018-05-14 09:36:37 -0300

    pontos de denuncia no mapa

--- web/index.js
+++ web/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express(); //"app" criado para se usar o express
 var pg = require('knex')({client: 'pg'});
+var knexPostgis = require('knex-postgis');
 var session = require('express-session');
 var md5 = require('md5');
 var consign = require('consign');
@@ -38,6 +39,12 @@ var knex = require('knex')({
     database : 'projetoMDS'
   }
 });
+
+const st = knexPostgis(knex);
+
+// var result = knex("denuncia").select(st.x('den_local_desordem'), st.y('den_local_desordem')).then(function(result){
+// 	console.log(result[0].den_local_desordem);
+// });
 
 
 // consign().include('routes').then();
@@ -96,13 +103,25 @@ app.post("/login", function(req,res){
 app.get("/admin", function(req,res){
 	sess = req.session;
 
+
 	if (sess.email) {
-		res.render('admin');
+		
+		knex.raw('select ST_X(den_local_desordem),ST_Y(den_local_desordem), den_status from denuncia').then(function(result){
+
+			res.render('admin', {pontos : result.rows});
+		});
+
 	}
 	else{
 		res.render("login", {failed : 0});
 	}
 });
+
+// GeoJSON Feature Collection
+function FeatureCollection(){
+    this.type = 'FeatureCollection';
+    this.features = new Array();
+}
 
 app.use(orgaoRouter);
 app.use(desordemRouter);
